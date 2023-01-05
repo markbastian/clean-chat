40c10acd950ddbad6231b993f600765a3ae225ef
Mark Bastian <markbastian@gmail.com>
2023-01-05 13:10:05 -0700

    Added tests

--- test/clean_chat/ex04_isolate_notifications/events_test.clj
+++ test/clean_chat/ex04_isolate_notifications/events_test.clj
@@ -1,11 +1,9 @@
 (ns clean-chat.ex04-isolate-notifications.events-test
   (:require [clojure.test :refer :all])
   (:require
-    [clean-chat.ex04-isolate-notifications.client-api :as client-api]
     [clean-chat.ex04-isolate-notifications.test-client-api]
     [clean-chat.ex04-isolate-notifications.config :as config]
     [clean-chat.ex04-isolate-notifications.commands :as commands]
-    [clean-chat.ex04-isolate-notifications.broker :as broker]
     [clean-chat.ex04-isolate-notifications.test-notifications]
     [clean-chat.ex04-isolate-notifications.events :refer :all]
     [datascript.core :as d]))
@@ -22,24 +20,29 @@
               {:event :enter-room :room-name "public" :username "Mark"}]
              events)))))
 
-(deftest join-chat-broker-test
-  (testing "Correct broker behavior when joining chat"
-    (let [clients (atom {})
-          res (atom [])
-          _ (client-api/add-client! clients {:client-id "Mark"
-                                             :transport :test
-                                             :res       res
-                                             :transform :test})
-          _ (broker/process-command
-              {:conn    (d/create-conn config/chat-schema)
-               :clients clients}
-              {:command   :join-chat
-               :username  "Mark"
-               :room-name "public"})]
+(deftest leave-chat-commands-test
+  (testing "Correct commands behavior when joining chat"
+    (let [conn (d/create-conn config/chat-schema)
+          join-events (commands/dispatch-command
+                        {:conn conn}
+                        {:command   :join-chat
+                         :username  "Mark"
+                         :room-name "public"})
+          leave-events (commands/dispatch-command
+                         {:conn conn}
+                         {:command  :leave-chat
+                          :username "Mark"})]
       (is (= [{:event :join-chat :username "Mark"}
               {:event :create-room :room-name "public"}
               {:event :enter-room :room-name "public" :username "Mark"}]
-             @res)))))
-
-
-
+             join-events))
+      (is (= [{:event :leave-room, :room-name "public" :username "Mark"}
+              {:event :leave-chat, :username "Mark"}]
+             leave-events))))
+  (testing "Leaving without being present is a null-op"
+    (let [conn (d/create-conn config/chat-schema)
+          events (commands/dispatch-command
+                   {:conn conn}
+                   {:command  :leave-chat
+                    :username "Mark"})]
+      (is (nil? events)))))
